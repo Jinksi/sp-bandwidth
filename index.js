035863ebe60a74ad2f61c23a748b29c22f81e496
Eric Jinks <ericjinks@gmail.com>
2017-03-28 11:48:13 +1000

    Init

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+const logDir = 'logs'
+const currentDir = fs.readdirSync('.')
+if (currentDir.indexOf(logDir) === -1) {
+  console.log(chalk.bgRed(`No ./${logDir} directory found`))
+}
+
+const files = fs.readdirSync('./' + logDir)
+
+const output = files.map(readFile)
+
+function readFile (file) {
+  const obj = JSON.parse(fs.readFileSync(`./${logDir}/${file}`, 'utf8'))
+
+  const title = file.split('.')[0]
+  const {
+    start_date,
+    end_date,
+    bandwidth
+  } = obj.general
+
+  const output = `
+    ${chalk.green.bold(title)} – ${chalk.yellow.underline(bytesToSize(bandwidth))} – From: ${chalk.blue(start_date)} To: ${chalk.blue(end_date)}
+  `
+  return output
+}
+
+console.log(output.join())
+
+function bytesToSize(bytes) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  if (bytes == 0) return '0 Byte'
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
